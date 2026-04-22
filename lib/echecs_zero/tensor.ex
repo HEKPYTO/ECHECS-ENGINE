ace0b00e69ffc96f49046f03abbb7ad929face3d
HEKPYTO <zoonyanapat@gmail.com>
2026-04-22 10:28:05 +0700

    feat: add EchecsEngine.Tensor for Nx representation

---
+++ lib/echecs_zero/tensor.ex
@@ -0,0 +1,42 @@
+defmodule EchecsZero.Tensor do
+  @moduledoc """
+  Adapter for converting Echecs.Game structs to Nx.Tensors.
+  """
+
+  import Bitwise
+
+  @doc """
+  Converts an Echecs.Game to an Nx.Tensor.
+  Returns a tensor of shape {1, 119, 8, 8}.
+  """
+  def to_tensor(%Echecs.Game{board: board}) do
+    board_tuple =
+      if is_tuple(board) do
+        board
+      else
+        Echecs.Board.from_struct(board)
+      end
+
+    # Extract the 12 piece bitboards
+    bitboards =
+      Tuple.to_list(board_tuple)
+      |> Enum.take(12)
+
+    # Convert bitboards to binary representations of 64 bytes each
+    layers_bin =
+      bitboards
+      |> Enum.map(&bitboard_to_binary/1)
+      |> Enum.join()
+
+    # Pad with zeros up to 119 layers
+    padding = :binary.copy(<<0>>, (119 - 12) * 64)
+    full_bin = layers_bin <> padding
+
+    Nx.from_binary(full_bin, {:u, 8})
+    |> Nx.reshape({1, 119, 8, 8})
+  end
+
+  defp bitboard_to_binary(val) do
+    for i <- 0..63, into: <<>>, do: <<val >>> i &&& 1::8>>
+  end
+end
---
+++ test/echecs_zero/tensor_test.exs
@@ -0,0 +1,35 @@
+defmodule EchecsZero.TensorTest do
+  use ExUnit.Case
+  doctest EchecsZero.Tensor
+
+  alias EchecsZero.Tensor
+
+  describe "to_tensor/1" do
+    test "converts standard starting board to tensor of shape {1, 119, 8, 8}" do
+      game = Echecs.new_game()
+      tensor = Tensor.to_tensor(game)
+
+      assert Nx.shape(tensor) == {1, 119, 8, 8}
+      assert Nx.type(tensor) == {:u, 8}
+
+      # For the starting position, check white pawns
+      white_pawns = tensor[0][0]
+      row_6 = white_pawns[6] |> Nx.to_flat_list()
+      assert row_6 == [1, 1, 1, 1, 1, 1, 1, 1]
+
+      black_pawns = tensor[0][6]
+      row_1 = black_pawns[1] |> Nx.to_flat_list()
+      assert row_1 == [1, 1, 1, 1, 1, 1, 1, 1]
+    end
+
+    test "handles board as struct" do
+      game = Echecs.new_game()
+      # Force board to be a struct
+      struct_board = Echecs.Board.to_struct(game.board)
+      game = %{game | board: struct_board}
+
+      tensor = Tensor.to_tensor(game)
+      assert Nx.shape(tensor) == {1, 119, 8, 8}
+    end
+  end
+end
